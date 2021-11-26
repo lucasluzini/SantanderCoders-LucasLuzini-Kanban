ab45fc26d8e8e02be29a4290d99e3c4cefff1fc2
Lucas Luzini <lucasluzini@hotmail.com>
2021-11-26 17:42:52 -0300

    4

--- FRONT/src/app/app-constants.ts
+++ FRONT/src/app/app-constants.ts
@@ -1,5 +1,5 @@
 export class AppConstants {
-    public static get baseServidor(): string { return "http://0.0.0.0:5000/"}
+    public static get baseServidor(): string { return "http://127.0.0.1:5000/"}
     public static get baseLogin(): string { return this.baseServidor + "login/"}
     public static get baseCards(): string { return this.baseServidor + "cards/"}
 }
--- FRONT/src/app/service/login-service.service.ts
+++ FRONT/src/app/service/login-service.service.ts
@@ -10,20 +10,19 @@ export class LoginServiceService {
   constructor(private http: HttpClient) {}
 
   login(usuario: any){
-    console.info(JSON.stringify(usuario))
+    //console.info(JSON.stringify(usuario))
 
     let headers: HttpHeaders = new HttpHeaders();
+
     headers = headers.append('Content-Type', 'application/json');
     headers = headers.append('Accept', 'application/json');
-    headers = headers.append('Access-Control-Allow-Origin', '*');
-    // headers = headers.append('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
-    // headers = headers.append('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
-//
-    // headers = headers.append('zumo-api-version', '2.0.0');
+    headers = headers.append('Access-Control-Allow-Origin', 'http://127.0.0.1:5000/');
+    headers = headers.append('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+    headers = headers.append('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
 
-    console.log({headers});
+    //console.log({headers: headers});
 
-    return this.http.post(AppConstants.baseLogin, JSON.stringify(usuario), {headers}).subscribe(data => {
+    return this.http.post(AppConstants.baseLogin, JSON.stringify(usuario), {headers: headers}).subscribe(data => {
       // Retorno Http
       console.info(JSON.parse(JSON.stringify(data)));
     })
